edbc0ace8aaa4ce133863efee72369a508a23d1e
alrvivas <alr.vivas@gmail.com>
2016-03-11 17:05:57 -0600

    v0.1

--- productos/urls.py
+++ productos/urls.py
@@ -5,6 +5,7 @@ urlpatterns = patterns('',
 	url(r'^add-categoria/$', 'productos.views.add_categoria', name='add-categoria'),
 	url(r'^categoria/(?P<categoria_slug>[-\w]+)/$','productos.views.ver_categoria', name='categoria'), 
 	url(r'^producto/(?P<producto_slug>[-\w]+)/$','productos.views.producto', name='producto'),
+	url(r'^add-producto/$', 'productos.views.add_producto', name='add-producto'),
 	#url(r'^inventario/$', 'productos.views.inventario', name='inventario'),
 	#url(r'^rescatar-inventario/$', 'productos.views.rescatar_inventario', name='rescatar-inventario'),
 
