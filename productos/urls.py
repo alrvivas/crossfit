0be38e83cc6f5af682520078ab174fed42341384
alrvivas <alr.vivas@gmail.com>
2016-03-11 12:50:50 -0600

    v0.1

--- productos/urls.py
+++ productos/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
 	url(r'^$','producto.views.categorias', name='categorias'),
+	url(r'^categorias/$', 'producto.views.ver_categoria', name='categorias'),
 	url(r'^add-categoria/$', 'producto.views.add_categoria', name='add-categoria'),
 	url(r'^categoria/(?P<categoria_slug>[-\w]+)/$','productos.views.ver_categoria', name='categoria'), 
 	url(r'^producto/(?P<producto_slug>[-\w]+)/$','productos.views.producto', name='producto'),
