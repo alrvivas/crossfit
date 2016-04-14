14b0fd18af7d5ce391d9a68d9eefd8cea76f1042
alrvivas <alr.vivas@gmail.com>
2016-04-14 12:19:42 -0500

    v0.1

--- ventas/urls.py
+++ ventas/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
 	url(r'^$', 'ventas.views.punto_venta', name='punto-venta'),
-	url(r'^ordenes-exitosa/$', 'ventas.views.orden_exitosa', name='orden-exitosa'),	
+	url(r'^ordenes-exitosa/(?P<orden_id>[-\w]+)$', 'ventas.views.orden_exitosa', name='orden-exitosa'),	
 	
 )
