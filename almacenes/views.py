5ce30f436f08684d6065af506f89d97c70642351
alrvivas <alr.vivas@gmail.com>
2016-04-25 16:11:20 -0500

    v0.2

--- almacenes/views.py
+++ almacenes/views.py
@@ -100,7 +100,7 @@ def entregar_orden(request,orden_id):
             return redirect(orden.get_absolute_url())
     else:
         form_orden = entregarodenForm()
-        formset = ProductoFormSet(queryset=Producto.objects.none())
+        formset = ProductoFormSet(queryset=Producto.objects.all())
     args = {}
     args.update(csrf(request))
     template_name = "entregar-orden.html"
